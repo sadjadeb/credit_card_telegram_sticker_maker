f4288c1e5bdcf6abc3815aaa46d0f7a8638307c8
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2022-04-04 16:50:12 +0430

    Create Django model to store bot users

--- sticker_maker/admin.py
+++ sticker_maker/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import BotUser
+
+admin.site.unregister(Group)
+admin.site.site_header = "Sticker Maker Admin"
+
+
+@admin.register(BotUser)
+class BotUserAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    ordering = ('-join_date',)
+    list_display = ('id', 'user_id', 'username', 'first_name', 'last_name', 'sticker_set', 'join_date')
+    search_fields = ('username', 'first_name', 'last_name')
+
+    def sticker_set(self, obj):
+        url = f'https://t.me/addstickers/cc_{obj.user_id}_by_credit_card_sticker_bot'
+        return format_html('<a href="{}">sticker</a>', url)
+
+    sticker_set.short_description = 'Sticker Set'
--- sticker_maker/models.py
+++ sticker_maker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BotUser(models.Model):
+    class Meta:
+        verbose_name = 'Bot User'
+        verbose_name_plural = 'Bot Users'
+
+    user_id = models.BigIntegerField(unique=True, verbose_name='Telegram ID')
+    first_name = models.CharField(max_length=127, verbose_name='First Name')
+    last_name = models.CharField(max_length=127, verbose_name='Last Name', blank=True, null=True)
+    username = models.CharField(max_length=64, verbose_name='Username', blank=True, null=True)
+    join_date = models.DateTimeField(auto_now_add=True, verbose_name='Join Date')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - @{self.username}'
