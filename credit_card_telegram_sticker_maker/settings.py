7dccd96fe5f8a25360f566ec304232d7800580d2
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2022-04-04 18:59:46 +0430

    Change time zone to Asia/Tehran

--- credit_card_telegram_sticker_maker/settings.py
+++ credit_card_telegram_sticker_maker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ud3nz1qu0bv-np_2!xc+!w^bavy#nngtfhk7#he024it#^(wbn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
